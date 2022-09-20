ab61b28f434d43f3d48889ca69d9eeab736efc35
Ameena Mashaikh <ameenamashaikh@gmail.com>
2022-09-20 10:07:57 -0400

    fixed colors header and checkboxes work. Need to find a way to get multiple values of checkbox selected

--- src/components/eyeshadow/FeaturesForm.js
+++ src/components/eyeshadow/FeaturesForm.js
@@ -1,3 +1,4 @@
+import { click } from "@testing-library/user-event/dist/click"
 import { useState, useEffect } from "react"
 import { useNavigate } from "react-router-dom"
 import { Link } from "react-router-dom"
@@ -10,9 +11,26 @@ export const FeaturesForm = () => {
 
     const [features, updateFeatures] = useState({
         "eyeColorId": 0,
-        "toneId": 0,
-        "userId": 0
+        "toneId": 0
     })
+
+
+
+    const [colorCategory, setColorCategory] = useState([])
+    
+    const [eyeshadowColors, setEyeshadowColors] = useState([])
+
+    const [eyeshadowEyeColor, setEyeshadowEyeColor] = useState([])
+
+    const [colorsBasedOnSelections, updateColorsBasedOnSelections] = useState({
+        "eyeshadowEyeColorId": 0,
+        "featuresSelectionId": 0
+    })
+
+    const [filteredEyeshadows, setFilteredEyeshadows] = useState([])
+
+
+    const [showEyeshadows, setShowEyeshadows] = useState(false)
 
     const navigate = useNavigate()
 
@@ -20,20 +38,20 @@ export const FeaturesForm = () => {
     const eyeUserObject = JSON.parse(localEyeUser)
 
 
-
+    
     useEffect(
         () => {
-            fetch(`http://localhost:8088/eyeColor`)
+            fetch(`http://localhost:8088/eyeColors`)
             .then(response => response.json())
             .then((eyeColorsArray) => {
                 setEyeColors(eyeColorsArray)
             })
         }, []
     )
-
+    
     useEffect(
         () => {
-            fetch(`http://localhost:8088/undertone`)
+            fetch(`http://localhost:8088/undertones`)
             .then(response => response.json())
             .then((undertonesArray) => {
                 setUndertones(undertonesArray)
@@ -41,15 +59,66 @@ export const FeaturesForm = () => {
         }, []
     )
 
+    useEffect(
+        () => {
+            fetch(`http://localhost:8088/colorCategories`)
+            .then(response => response.json())
+            .then((colorCategoryArray) => {
+                setColorCategory(colorCategoryArray)
+            })
+        }, []
+        )
+        
+        
+        useEffect(
+            () => {
+                fetch(`http://localhost:8088/eyeshadowColors?_expand=colorCategory`)
+                .then(response => response.json())
+                .then((eyeshadowArray) => {
+                    setEyeshadowColors(eyeshadowArray)
+                })
+            }, []
+            )
+            
+            
+        useEffect(
+            () => {
+                fetch(`http://localhost:8088/eyeshadowEyeColors?_expand=eyeColor&_expand=eyeshadowColor`)
+                .then(response => response.json())
+                .then((data) => {
+                    setEyeshadowEyeColor(data)
+                })
+            }, []
+            )
+        
+        useEffect(() => {
+
+           const eyeshadowMatch = eyeshadowEyeColor.filter(eyeshadow => {
+                    return ((features.toneId === eyeshadow.eyeshadowColor.toneId) && (features.eyeColorId === eyeshadow.eyeColorId)  )
+                })
+            
+            setFilteredEyeshadows(eyeshadowMatch)
+
+        }, [features])
+                
+
+
+
+
+
     const handleSaveButtonClick = (event) => {
 
         event.preventDefault()
         const featuresToSendToAPI = {
-            userId: eyeUserObject.id,
-            eyeColorId: features.eyeColorId,
-            toneId: features.toneId
+            userId: parseInt(eyeUserObject.id),
+            eyeColorId: parseInt(features.eyeColorId),
+            toneId: parseInt(features.toneId)
         }
-        return fetch(`http://localhost:8088/featuresSelection`, {
+
+        setShowEyeshadows(true)
+
+
+        return fetch(`http://localhost:8088/featuresSelections`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
@@ -58,18 +127,101 @@ export const FeaturesForm = () => {
         })
         .then(response => response.json())
         .then(() => { 
-
+  
         })
     }
 
 
-    const EyeshadowSelections = () => {
 
-        
+    const eyeshadowSaveButtonClick = (event) => {
+        event.preventDefault()
+        const colorsAndFeaturesToSendToAPI = {
+            eyeshadowEyeColorId: parseInt(colorsBasedOnSelections.eyeshadowEyeColorId),
+            featuresSelectionId: parseInt(features.id)
+        }
+        return fetch(`http://localhost:8088/colorsBasedOnSelections`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(colorsAndFeaturesToSendToAPI)
+        })
+        .then(response => response.json())
+        .then(() => { 
+  
+        })
     }
 
 
-   return (
+
+    
+
+
+
+
+    const eyeshadowSelections = () => {
+        
+        let html = []
+        if (showEyeshadows)
+        {
+            html.push(<>
+                <div>
+                    <h2 className="eyeshadow__title">Eyeshadow Colors Made For You</h2>
+                </div>
+                <div>
+                    <h3 className="colors__title"> Pick Out Your Favorite Eyeshadow Colors!</h3>
+                </div>
+                </>)
+            colorCategory.map(category => {
+                html.push(<h4>{category.color}</h4>)
+                filteredEyeshadows.map(eye => {
+                    if (eye.eyeshadowColor.colorCategoryId === category.id)
+                    {
+                        
+                        html.push(<label htmlFor="eyeshadowColor"><input 
+                        type = "checkbox" 
+                        key = {`eyeshadowEyeColor--${eye?.id}`}
+                        value = {eye?.id} 
+                        
+                            onChange={(event) => {
+                                const copy = {...colorsBasedOnSelections}
+                                copy.eyeshadowEyeColorId = event.target.value
+                                updateColorsBasedOnSelections(copy)
+                            }}/>
+                            {eye.eyeshadowColor?.name}
+                            </label>)
+                        
+                        
+                    }   
+                        
+                else {
+                    <div></div>
+                }
+            }
+            )
+        }
+        )
+            html.push(<div><button onClick = {(clickEvent) => {eyeshadowSaveButtonClick(clickEvent);}} className="btn btn-primary">
+                    Submit Eyeshadows
+                </button></div>)
+    }
+
+        else {
+            <div></div>
+        }
+        
+        
+        
+        return html
+    }
+        
+            
+            
+    
+    
+    
+    
+    return (
         <form className="featuresForm">
             <h2 className="featuresForm__title">Eyeshadow Color Generator</h2>
             <fieldset>
@@ -79,17 +231,17 @@ export const FeaturesForm = () => {
                     required autoFocus
                     className="form-control"
                     value={eyeColors.id}
-                        onChange={(event) => {
+                    onChange={(event) => {
                             const copy = {...features}
-                            copy.eyeColorId = event.target.value
+                            copy.eyeColorId = parseInt(event.target.value)
                             updateFeatures(copy)
                         }}>                
                         <option value = "0"> Choose Eye Color</option>        
                         {
-                        eyeColors.map(eyeColor => {
-                            return <option value = {eyeColor.id} key = {`eyeColor--${eyeColor.id}`}> {eyeColor.color}</option>
-                        })
-                    }
+                            eyeColors.map(eyeColor => {
+                                return <option value = {eyeColor.id} key = {`eyeColor--${eyeColor.id}`}> {eyeColor.color}</option>
+                            })
+                        }
                         </select>
                 </div>
             </fieldset>
@@ -101,25 +253,57 @@ export const FeaturesForm = () => {
                         className="form-control"
                         value={undertones.id}
                         onChange={(event) => {
-                                const copy = {...features}
-                                copy.toneId = event.target.value
-                                updateFeatures(copy)
-                            }
-                        }>
+                            const copy = {...features}
+                            copy.toneId = parseInt(event.target.value)
+                            updateFeatures(copy)
+                        }
+                    }>
                         <option value = "0"> Choose Undertone</option>
                         {
                             undertones.map(undertone => {
-                            return <option value = {undertone.id} key = {`undertone--${undertone.id}`}> {undertone.tone}</option>})
-                        }
+                                return <option value = {undertone.id} key = {`undertone--${undertone.id}`}> {undertone.tone}</option>})
+                            }
                         </select>
                         <section className="link--undertone">
                             Need help finding your undertone?   <Link to="/undertone_finder">Click Here!</Link>
                         </section>
                 </div>
             </fieldset>
-            <button onClick = {(clickEvent) => handleSaveButtonClick(clickEvent)}  className="btn btn-primary">
+            <button onClick = {(clickEvent) => {handleSaveButtonClick(clickEvent);}} className="btn btn-primary">
                 Submit Features
             </button>
+            {eyeshadowSelections()}
         </form>
     )
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// eyeshadowEyeColor.eyeColorId === eyeColors.id && eyeshadowEyeColor.eyeShadowColorId === eyeshadowColors.id
---
+++ src/components/undertone/UndertoneFinder.js
@@ -0,0 +1,19 @@
+import "./UndertoneFinder.css"
+
+
+
+export const UndertoneFinder = () => {
+    return (
+        <section className="undertoneFinder">
+            <h2 className="undertoneFinder__title">Undertone Finder</h2>
+            <div> Find Your Undertone!</div>
+            <img src={require('./undertonespng.png')} />
+            <section className = "undertone__descriptions">
+                <article className = "warmUndertone__description">
+                    <h3>Warm Undertone</h3>
+                    <div></div>
+                </article>
+            </section>
+        </section>
+    )
+}
